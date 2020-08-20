aba80716463c6ba1300dc835da4e323ee8cd2f5d
Kyle Jones <rmbart395@yahoo.com>
2020-08-20 13:41:55 -0600

    Add Round and Round test file

---
+++ src/Round.js
@@ -0,0 +1,31 @@
+const Turn = require('../src/Turn');
+
+class Round {
+  constructor(deck) {
+    this.deck = deck;
+    this.turns = 0;
+    this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    return this.deck.cards[this.turns];
+  }
+
+  takeTurn(guess) {
+    let turn = new Turn(guess, this.deck.cards[this.turns]);
+    this.turns++;
+    if (turn.evaluateGuess() == true) {
+      return turn.giveFeedback();
+    } else {
+      this.incorrectGuesses.push(turn.returnCard().id);
+      return turn.giveFeedback();
+    }
+  }
+
+  calculatePercentCorrect() {
+    let percentCorrect = this.incorrectGuesses.length / this.turns;
+    return percentCorrect * 100;
+  }
+}
+
+module.exports = Round;
